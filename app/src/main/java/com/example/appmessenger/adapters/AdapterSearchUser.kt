3e5c011af3072780ad3f26c5ccf8c2f9c43791bf
TrungChinhNguyen02 <danchoi113.tn@gmail.com>
2024-02-29 11:51:26 +0700

    commit sua

--- app/src/main/java/com/example/appmessenger/adapters/AdapterSearchUser.kt
+++ app/src/main/java/com/example/appmessenger/adapters/AdapterSearchUser.kt
@@ -1,10 +1,8 @@
 package com.example.appmessenger.adapters
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,40 +17,40 @@ import com.example.appmessenger.utils.FirebaseUtill
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
-class AdapterSearchUser(options: FirestoreRecyclerOptions<UserModel>,private val context: Context):
-        FirestoreRecyclerAdapter<UserModel, AdapterSearchUser.UserModelViewHolder>(options){
+class AdapterSearchUser(options: FirestoreRecyclerOptions<UserModel>, private val context: Context) :
+    FirestoreRecyclerAdapter<UserModel, AdapterSearchUser.UserModelViewHolder>(options) {
+
     private var firebaseUtill = FirebaseUtill()
-    inner class UserModelViewHolder(itemview: View): RecyclerView.ViewHolder(itemview){
-        val username: TextView = itemview.findViewById(R.id.search_name_text)
-        val emalText: TextView = itemview.findViewById(R.id.search_email_text)
-        val userpic: ImageView= itemview.findViewById(R.id.search_pic_image_view)
+
+    inner class UserModelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val username: TextView = itemView.findViewById(R.id.search_name_text)
+        val emailText: TextView = itemView.findViewById(R.id.search_email_text) // Đã thay đổi thành user_email_text
+        val userpic: ImageView = itemView.findViewById(R.id.search_pic_image_view)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserModelViewHolder {
-            val view = LayoutInflater.from(context).inflate(R.layout.search_user,parent,false)
+        val view = LayoutInflater.from(context).inflate(R.layout.search_user, parent, false)
         return UserModelViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserModelViewHolder, position: Int, model: UserModel) {
-        if (model.mUId == firebaseUtill.currentUserId()){
-            holder.username.text = model.mUserName + "(Me)"
-            holder.emalText.text = model.mEmail
-        }else{
-            holder.username.text = model.mUserName
-            holder.emalText.text = model.mEmail
+        holder.username.text = model.mUserName
+        holder.emailText.text = model.mEmail
+
+        if (model.mUId == firebaseUtill.currentUserId()) {
+            holder.username.text = model.mUserName + " (Me)"
         }
-//
-//        firebaseUtill.getCurrentProfilePicStorageRef().downloadUrl
-//            .addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    val uri: Uri? = task.result
-//                    if (uri != null) {
-//                        AndroidUtil().setProfilePic(context, uri, holder.userpic)
-//                    }
-//                }else{
-//                    Log.d("TAG", "onBindViewHolder: không tìm thấY ảnh đại diện ")
-//                }
-//            }
+
+        firebaseUtill.getCurrentProfilePicStorageRef().downloadUrl
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val uri: Uri? = task.result
+                    if (uri != null) {
+                        AndroidUtil().setProfilePic(context, uri, holder.userpic)
+                    }
+                }
+            }
+
         holder.itemView.setOnClickListener {
             //navigate to chat activity
             val intent = Intent(context, ChatActivity::class.java)
@@ -60,6 +58,5 @@ class AdapterSearchUser(options: FirestoreRecyclerOptions<UserModel>,private val
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             context.startActivity(intent)
         }
-
     }
 }
